bfa7e162a33a69b6bb7fad43688e85d0903a1c0a
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-11 18:15:52 +0900

    default "pending" in ticket status, and migrated db

--- db/migrate/20200711091007_create_tickets.rb
+++ db/migrate/20200711091007_create_tickets.rb
@@ -3,7 +3,7 @@ class CreateTickets < ActiveRecord::Migration[6.0]
     create_table :tickets do |t|
       t.references :user, null: false, foreign_key: true
       t.references :event, null: false, foreign_key: true
-      t.string :status
+      t.string :status, default: "pending"
 
       t.timestamps
     end
