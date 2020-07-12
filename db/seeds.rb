536406ca1c2965ccda481cd92e8fc84d67e2722e
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-13 01:09:37 +0900

    added faker gem, added simple seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,48 +12,40 @@ Event.destroy_all
 User.destroy_all
 puts 'Database Cleared.'
 
-puts 'Creating 10 new users...'
-puts '10 users created.'
-
-puts 'Creating 10 new events...'
-puts '10 events created.'
-
-puts 'Seeding completed. Have a nice day.'
-
-
 def create_users
   10.times do
     user = User.new(
-      email: ,
-      encrypted_password: ,
+      email: Faker::Internet.safe_email,
+      password: Faker::Internet.password(min_length: 10, max_length: 20),
     )
     user.save
+    create_events(user)
   end
 end
 
-def create_events
-  10.times do
+def create_events(user)
+  1.times do
     event = Event.new(
-      title: ,
-      description: ,
-      datetime: ,
-      venue: ,
-      capacity: ,
-      category:
+      title: Faker::Verb.ing_form,
+      description: "A bit long description of my great event! It'll be great!",
+      datetime: rand(Time.zone.now .. 4.days.from_now),
+      venue: Faker::Restaurant.name,
+      capacity: rand(2..8),
+      category: Faker::Restaurant.type
     )
+    event.user = user
     event.save
+    3.times do
+      ticket = Ticket.new(
+        event: event,
+        user: User.all.to_a.reject!{ |u| u == event.user}.sample
+      )
+      ticket.save
+    end
   end
 end
 
-def create_tickets
-  10.times do
-    ticket = Ticket.new(
-      user_id: ,
-      event_id:
-    )
-    ticket.save
-  end
-end
-
-
+puts 'Creating users, events, and tickets...'
+create_users
+puts 'Seeding completed. Have a nice day.'
 
