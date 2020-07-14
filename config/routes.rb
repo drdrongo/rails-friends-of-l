5de77677e9a0566ef1fe8bf2e0b8ec38b00934bc
ak569 <62170909+ak569@users.noreply.github.com>
2020-07-14 19:45:18 +0900

    added tickets

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :tickets, only: [:index, :new, :create, :edit, :destroy]
 end
