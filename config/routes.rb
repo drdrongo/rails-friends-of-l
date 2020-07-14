86932584ec8160934f872f1925642dd4e97b6e5c
ak569 <62170909+ak569@users.noreply.github.com>
2020-07-14 19:55:08 +0900

    nested tickets

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy]
-  resources :tickets, only: [:index, :new, :create, :edit, :destroy]
+  resources :events, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :tickets, only: [:create]
+   end
+  resources :tickets, only: [:update]
 end
