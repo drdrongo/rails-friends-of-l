c6ba72bad39e85336ee8104aa53a4add0956d120
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-16 19:17:13 +0900

    event controller actions part 2

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: %i[show edit update destroy]
 
   def index
-    @events = Event.all
+    @events = policy_scope(Event)
   end
 
   def show
@@ -15,17 +15,24 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
     authorize @event
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
   end
 
   def edit
+    authorize @event
   end
 
   def update
     authorize @event
 
     if @event.update(event_params)
-      redirect_to events_path
+      redirect_to event_path(@event)
     else
       render :edit
     end
@@ -33,14 +40,14 @@ class EventsController < ApplicationController
 
   def destroy
     authorize @event
-
     @event.destroy
+    redirect_to events_path
   end
 
   private
 
   def event_params
-    Params.require('event').permit(:title, :description, :datetime, :venue, :capacity, :category, :user)
+    Params.require('event').permit(:title, :description, :datetime, :venue, :capacity, :category, :user, :end_time)
   end
 
   def set_event
