7c0afff2770047d8da463bfebbe4980629dd542b
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-16 18:23:59 +0900

    mysterious end_time came outta nowhere man

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_100118) do
+ActiveRecord::Schema.define(version: 2020_07_16_092045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_100118) do
     t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "end_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
@@ -47,6 +48,8 @@ ActiveRecord::Schema.define(version: 2020_07_14_100118) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin"
+    t.string "name"
+    t.integer "age"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
