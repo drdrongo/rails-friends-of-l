f6ec33d2d293c8f7eb2f3c78b923aea6898ad19b
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-21 20:36:14 +0900

    user schema altered

---
+++ db/migrate/20200721112936_add_information_to_users.rb
@@ -0,0 +1,8 @@
+class AddInformationToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :birthday, :datetime
+    add_column :users, :description, :text
+    add_column :users, :last_name, :string
+    rename_column :users, :name, :first_name
+  end
+end
