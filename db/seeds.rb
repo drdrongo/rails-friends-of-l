121a99024ae84f53918882135cb8f06401cbcdb1
Hayato Clarke <hayatoclarke@gmail.com>
2020-07-21 20:45:45 +0900

    seeds altered

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,9 +15,12 @@ puts 'Database Cleared.'
 def create_users
   10.times do
     user = User.new(
-      name: Faker::Name.unique.first_name,
+      first_name: Faker::Name.unique.first_name,
+      last_name: Faker::Name.last_name,
       email: Faker::Internet.unique.safe_email,
       password: Faker::Internet.password(min_length: 10, max_length: 20),
+      description: "This is a great description of my personality! I'm a great person!",
+      birthday: rand(52.years.ago .. 20.years.ago)
     )
     file = URI.open("https://source.unsplash.com/featured/?face/adult&#{rand(10000)}")
     user.photo.attach(io: file, filename: "#{user.name}.png", content_type: 'image/png')
@@ -30,7 +33,7 @@ def create_events(user)
   1.times do
     event = Event.new(
       title: "#{Faker::Food.dish} with friends",
-      description: "A bit long description of my great event! It'll be great!",
+      description: "A bit of a long description of my great event! It'll be great!",
       datetime: rand(Time.zone.now .. 4.days.from_now),
       venue: Faker::Restaurant.name,
       capacity: rand(2..8),
@@ -41,11 +44,13 @@ def create_events(user)
     event.end_time = event.datetime + rand(1..3).hours
     event.user = user
     event.save
+    unused_users = User.all.to_a.reject!{ |u| u == event.user}
     6.times do
       ticket = Ticket.new(
         event: event,
-        user: User.all.to_a.reject!{ |u| u == event.user}.sample
+        user: used_users.sample
       )
+      unused_users.drop(ticket.user.id)
       ticket.status = ['pending', 'accepted'].sample
       ticket.save
     end
